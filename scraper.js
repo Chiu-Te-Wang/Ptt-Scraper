638d0fad93bf1312342eb6d1fadaf3fb9f884fc0
Chiu-Te-Wang <qwas65166516@gmail.com>
2014-05-24 16:48:26 +0800

    commit by Chiu 2014/05/24

--- scraper.js
+++ scraper.js
@@ -9,134 +9,127 @@ var contentCounter = 0;//for file system ,ensure contents be scrape
 var particleNum = 0;
 var newPage = 'http://www.ptt.cc/bbs/Food/index.html';
 var newestIdex = 0;
-var pages = 3;//496
-
+var pages = 38;//496
+var scrapLoopNum = 1;
 //need to put in worker dyno and set setTimeout
 
-var pttLimit = 2;
+var pttLimit = 29;
 var divider = parseInt(pages/pttLimit);
 var remain  = pages - divider*pttLimit;
 console.log("divider is " + divider);
 console.log("remain is " + remain);
-var recurCount = 0;
+var recurCount = 0;//use in function scrapeContent
 
 var whileCount = 0;
-
-findNewest();
-async.whilst(
-	function(){return (whileCount < 2);},
-	function(callback){
-		async.waterfall([
-			function(callback1){
-				setTimeout(function(){
-				if(!(newestIdex === 0)){
-					// for(var i = 0; i<remain;i++){
-					// 	scrapeFunc(newestIdex-i,i);
-					// 	console.log("pages "+(newestIdex-i)+" done "+i + " @@ "+parseCounter);
-					// 	setTimeout(function(){},50);
-					// }
-					forSetTime3(0);
-				}
-				},2000);
-				callback1(null);
-			},
-
-			function(callback1){
-				forSetTime(0);
-				callback1(null);
-			},
-		// for(var j = 0; j<parseInt((pages-remain)/pttLimit);j++){
-		// 	console.log("enter j!!!! " + j);
-		// 	(function(j){
-		// 	setTimeout(function(){
-		// 		console.log("ctw " + j);
-		// 		if(!(newestIdex === 0)){
-		// 			for(var i = 0; i<pttLimit;i++){
-		// 				(function(i){
-		// 				scrapeFunc((newestIdex-pttLimit*j-i-remain),(i+pttLimit*j+remain));
-		// 				// console.log("pages "+(newestIdex-i-pttLimit*j-remain)+" done? "+(i+pttLimit*j+remain) + " @@ "+parseCounter);
-		// 				setTimeout(function(){},500*i);
-		// 				})(i);
-		// 			}
-		// 		}
-		// 	},j*500);
-		// 	})(j);
-		// }
-			function(callback1){
-				setTimeout(function(){console.log("!!##!! "+parseCounter);},2000);
-				whileCount++;
-				newestIdex = newestIdex - (whileCount*(pages-1)*20+ pttLimit);
-				
-				callback1(null);
-			}],function(err,results){
-				if(err){console.log("err from waterfall: "+err);}
-			}
-		);
-		setTimeout(callback,0);
+async.series([
+	function(callback1){
+		setTimeout(function(){
+			//findNewestPage();
+			newestIdex = 2699;
+			callback1();
+		},3000);
 	},
-	function(err){
-		if(err){
-			console.log("Err: "+err);
-		}
+
+	function(callback1){
+		setTimeout(function(){
+			loopScrape();
+			callback1();
+		},0);
 	}
-);
 
-
+],function(err,re){
+	if(err){console.log("err: from outer " + err);}
+});
 
 
 
 
 //----------------------------functions-------------------------------
-function findNewest(){//get the newest pages number
+function findNewestPage(){//get the newest pages number
 	request(newPage,function(err,res,html){
 	if(!err && res.statusCode === 200){
 		var $ = cheerio.load(html);
 		var a = $('.btn-group.pull-right').children('.btn.wide').eq(1);
 		newestIdex = parseInt(a.attr('href').replace(/(\/|bbs|Food|index|\.html)/gm,""))+1;
-		console.log(newestIdex);
+		console.log("newestIdex in findNewestPage : "+newestIdex);
 	}
 });
 }
 
-function forSetTime(j){
-	if(j<parseInt((pages-remain)/pttLimit)){
-		 console.log("enter j!!!! " + j);
+function Scraper4Auto(out_Cout){
+	if(out_Cout<parseInt((pages-remain)/pttLimit)){
+		 console.log("enter Scraper4Auto!!!! " + out_Cout);
 		setTimeout(function(){
-			console.log("ctw " + j);
-			if(!(newestIdex === 0)){
-				forSetTime2(j,0);
-			}
-			j++;
-			forSetTime(j);
+			async.series([
+				function(callback){
+					setTimeout(function(){
+						console.log("ctw " + out_Cout);
+						if(!(newestIdex === 0)){
+							_Scraper4Auto(out_Cout,0);
+						}
+						out_Cout++;
+						callback();
+					},0);
+				},
+
+				function(callback){
+					setTimeout(function(){
+						Scraper4Auto(out_Cout);
+						callback();
+					},0);
+					
+				}
+			
+			],function(err){});
 		},5000);
 	}
 	else{//out of for loop
-				 console.log("out of j!!!");
+				 console.log("out of out_Cout!!!");
 	}
 }
 
-function forSetTime2(j,i){
-	if(i<pttLimit){
-		 console.log("pages "+(newestIdex-i-pttLimit*j-remain)+" done? "+(i+pttLimit*j+remain) + " @@ "+parseCounter);
+function _Scraper4Auto(out_Cout,inner_Count){
+	if(inner_Count<pttLimit){
+		 console.log("pages "+(newestIdex-inner_Count-pttLimit*out_Cout-remain)+" done? "+(inner_Count+pttLimit*out_Cout+remain) + " @@ "+parseCounter);
 		setTimeout(function(){
-			scrapeFunc((newestIdex-pttLimit*j-i-remain),(i+pttLimit*j+remain));
-			i++;
-			 console.log("---------in i!!!");
-			forSetTime2(j,i);
-		},5000+i*10);
+			scrapeFunc((newestIdex-pttLimit*out_Cout-inner_Count-remain),(inner_Count+pttLimit*out_Cout+remain),"_Scraper4Auto");
+			inner_Count++;
+			 console.log("---------in _Scraper4Auto!!!");
+			 console.log("newestIdex in _Scraper4Auto : "+newestIdex);
+			_Scraper4Auto(out_Cout,inner_Count);
+		},5000+inner_Count*10);
 	}
 	else{//out of for loop
-		 console.log("=======out of i!!!");
+		 console.log("=======out of _Scraper4Auto!!!");
 	}
 }
 
-function forSetTime3(num){
+function Scraaper4Remain(num){
 	if(num<remain){
 		setTimeout(function(){
-			scrapeFunc(newestIdex-num,num);
-			console.log("pages "+(newestIdex-num)+" done "+num + " @@ "+parseCounter);
-			num++;
-			forSetTime3(num);
+			async.series([
+				function(callback){
+					setTimeout(function(){
+						scrapeFunc(newestIdex-num,num,"Scraaper4Remain");
+						callback();
+					},0);
+				},
+
+				function(callback){
+					setTimeout(function(){
+						console.log("pages "+(newestIdex-num)+" done "+num + " @@ "+parseCounter);
+						console.log("newestIdex in Scraaper4Remain : "+newestIdex);
+						num++;
+						callback();
+					},0);
+				},
+
+				function(callback){
+					setTimeout(function(){
+						Scraaper4Remain(num);
+						callback();
+					},0);
+				}],function(err){});
 		},5000)
 	}
 	else{
@@ -144,63 +137,23 @@ function forSetTime3(num){
 	}
 }
 
-function scrapeFunc(index,status){ //status means newest comming(first page)
+function scrapeFunc(index,status,who){ //status means newest comming(first page)
 	var web = 'http://www.ptt.cc/bbs/Food/index'+index+'.html';
 	console.log(web);
 	request(web,function(err,res,html){
 		if(!err && res.statusCode === 200){
 			var $ = cheerio.load(html);
-			if(status === 0){particleNum = $('div.r-ent').length; console.log("1st: "+particleNum);}
-				// $('div.r-ent').each(function(i,element){
-				// 	// parseCounter++;
-				// 	var title = $(this).children('.title').text();
-				// 	var date = $(this).children('.meta').children('.date').text();
-				// 	var a = $(this).children('.title').children('a');
-				// 	//scrape content
-				// 	var contentURL = 'http://www.ptt.cc/' + a.attr('href');
-				// 	var content = [''];
-				// 	var metaData = {};
-				// 	async.series({
-				// 		one: function(callback){
-				// 			setTimeout(function(){
-				// 				recurCount = 0;
-				// 			scrapeContent(contentURL,content);
-				// 			console.log(content); 
-				// 			},1000);
-				// 			console.log('content is :' + content[0]);
-				// 			setTimeout(function(){callback()},3000);
-				// 		},
-
-				// 		//save as json type
-				// 		two: function(callback){
-				// 			metaData = {
-				// 				tag: true,
-				// 				title: title.replace(/(\t|\n)/gm,""),
-				// 				date: date.replace(/(\t|\n| )/gm,""),
-				// 				content: content[0].replace(/(\t|\n| )/gm,"")
-				// 			};
-				// 			callback();
-				// 		}},
-				// 		function(err,results){
-
-				// 			parsedResults.push(metaData);
-				// 			console.log(parseCounter+"  "+date.replace(/(\t|\n| )/gm,"") + " c: "+contentCounter);
-				// 			parseCounter++;
- 
-				// 			if(parseCounter === (particleNum + (pages-1)*20) 
-				// 			   && contentCounter === (particleNum + (pages-1)*20)){ 
-				// 			   //ptt has 20 articles per page
-				// 				writeData();
-				// 			}
-				// 		});
-
-				// });
-_scrapeFunc(0,html);
+			if(status === 0){
+				particleNum = $('div.r-ent').length; 
+				console.log("1st: "+particleNum);
+			}	
+			//scrape Func
+			_scrapeFunc(0,html,who);
 		}
 	});
 }
 
-function scrapeContent(URL,content){
+function scrapeContent(URL,content,who){
 	if(URL === 'http://www.ptt.cc/undefined'){
 		//because the article be deleted ,so no URL 
 		contentCounter++;
@@ -221,8 +174,9 @@ function scrapeContent(URL,content){
 								.end()
 								.text()
 								;
-						console.log("Scrape : "+ URL);
+						console.log(who + " Scrape : "+ URL);
 						recurCount = 0;
+						console.log("call scrapeContent!!");
 			}
 			else{
 				//contentCounter++;
@@ -230,12 +184,15 @@ function scrapeContent(URL,content){
 				//console.log("recurCount: "+recurCount);
 				if(recurCount<5){
 					//console.log('try: '+recurCount);
-					setTimeout(scrapeContent(URL,content),1500+recurCount);
+					setTimeout(scrapeContent(URL,content,who),1500+recurCount);
+					console.log("call scrapeContent!! rere");
 				}
 				else
 				{//Error : 503 or 500 or 404
-					contentCounter++
-					console.log("err: " + res.statusCode+" URL = "+URL);
+					contentCounter++;
+					if(res){
+						console.log("err: " + res.statusCode+" URL = "+URL);
+					}
 					setTimeout(function(){},2000);
 					recurCount = 0;
 					content[0] = "error"
@@ -253,13 +210,17 @@ function writeData(){
 			    		throw err;
 			    	}
 			    	console.log('Data saved!!  length: ' + parsedResults.length+' count: '+contentCounter);
+			    	console.log('newestIdex: '+newestIdex);
+			    	newestIdex = newestIdex - pages;
+			    	parseCounter = 0;
+			    	contentCounter = 0;
 			    });
 }
 
 
 
 
-function _scrapeFunc(num,html){
+function _scrapeFunc(num,html,who){
 	var $ = cheerio.load(html);
 	// console.log("debugger : "+($('div.r-ent').length));
 	if(num< $('div.r-ent').length){
@@ -276,38 +237,84 @@ function _scrapeFunc(num,html){
 						one: function(callback){
 							setTimeout(function(){
 								recurCount = 0;
-							scrapeContent(contentURL,content);
+								scrapeContent(contentURL,content,who);
+								// console.log('content is :' + content[0]);
+								setTimeout(function(){callback();},3000);
 							},1000);
-							// console.log('content is :' + content[0]);
-							setTimeout(function(){callback();},3000);
 						},
 
 						//save as json type
 						two: function(callback){
-							metaData = {
-								tag: true,
-								title: title.replace(/(\t|\n)/gm,""),
-								date: date.replace(/(\t|\n| )/gm,""),
-								content: content[0].replace(/(\t|\n| )/gm,"")
-							};
-							callback();
+							setTimeout(function(){
+								metaData = {
+									tag: true,
+									title: title.replace(/(\t|\n)/gm,""),
+									date: date.replace(/(\t|\n| )/gm,""),
+									content: content[0].replace(/(\t|\n| )/gm,"")
+								};
+
+								parsedResults.push(metaData);
+								console.log(parseCounter+"  "+date.replace(/(\t|\n| )/gm,"") + " c: "+contentCounter);
+								parseCounter++;
+								if((parseCounter === (particleNum + (pages-1)*20) || parseCounter >= (particleNum + (pages-5)*20)) 
+									&& (parseCounter<(particleNum + (pages-1)*20))){ 
+							    //ptt has 20 articles per page
+								    if(parseCounter === (particleNum + (pages-1)*20)){
+										writeData();
+									}
+									else{
+										writeData();
+										parseCounter = (particleNum + (pages-1)*20)+5;
+									}
+
+								}
+
+								num++;
+								_scrapeFunc(num,html,who);
+								callback();
+							},500);
 						}},
 						function(err,results){
-
-							parsedResults.push(metaData);
-							console.log(parseCounter+"  "+date.replace(/(\t|\n| )/gm,"") + " c: "+contentCounter);
-							parseCounter++;
- 
-							if(parseCounter === (particleNum + (pages-1)*20) 
-							   && contentCounter === (particleNum + (pages-1)*20)){ 
-							   //ptt has 20 articles per page
-								writeData();
-							}
-
 							if(err){console.log("err : "+err);}
 						});
-					num++;
-					_scrapeFunc(num,html);
 					},1000);
 	}
 }
+
+function loopScrape(){
+			if(whileCount < scrapLoopNum){
+				setTimeout(function(){
+					async.series([
+						function(callback3){
+							setTimeout(function(){
+								//scrape first page
+								if(!(newestIdex === 0)){
+									Scraaper4Remain(0);
+									setTimeout(callback3(null),200);
+								}
+								
+							},2000);
+						},
+
+						function(callback3){
+							setTimeout(function(){
+								//scrape other pages
+								Scraper4Auto(0);
+								setTimeout(callback3(null),200);
+							},2000);
+						},
+
+						function(callback3){
+							setTimeout(function(){
+								console.log("!!##!! "+parseCounter);
+								whileCount++;
+								setTimeout(callback3(null),200);
+							},2000);
+					}],function(err,re){loopScrape();});
+				},0);
+		}
+		else{
+			//end loop 
+			console.log("out of loop!!");
+		}
+	}
